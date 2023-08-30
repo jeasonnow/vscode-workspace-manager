6000b7e5567933e0f19ff0370c186dd1d8f517ff
顾一峰 <guyf@seeyon.com>
2023-08-30 17:34:58 +0800

    ✨ First Version for VSCode workspace manager

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,34 @@
-import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { List } from "@raycast/api";
+import {lstatSync, readdirSync} from 'fs-extra';
+import {join} from 'path';
+import LocalItem from "./components/LocalItem";
 
-export default function Command() {
-  return (
-    <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
-    </List>
-  );
+import { workspacePath } from './preference';
+
+
+function VisualStudioCodeWorkspaceManager() {
+  const WORKSPACE_DIR = workspacePath;
+    
+  const directoryFilter = (filename: string) => {
+      return lstatSync(join(WORKSPACE_DIR, filename)).isDirectory();
+  };
+  
+  const dirs = readdirSync(join(WORKSPACE_DIR)).filter(directoryFilter);
+  const projects = dirs.map(item => join(WORKSPACE_DIR, item));
+    
+
+  return <List
+    searchBarPlaceholder="Search Project Name..."
+  >
+    <List.Section title="Result">
+      {
+        projects.map(project => {
+          return <LocalItem key={project} project={project} />
+        })
+      }
+    </List.Section>
+
+  </List>
 }
+
+export default VisualStudioCodeWorkspaceManager;
